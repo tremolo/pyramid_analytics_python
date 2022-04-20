8f3b2054e169d1fafabd8ed2ef8433f27478021d
Sherman Wood <sgwood63@users.noreply.github.com>
2022-04-19 17:30:25 -0700

    debugging

--- pyramid_api/api_types.py
+++ pyramid_api/api_types.py
@@ -429,5 +429,14 @@ class ImportApiResultObject(DataClassJsonMixin):
     itemsIds: Dict[str, str] = default_field({})
 
     def __post_init__(self):
-        self.importDscMap = {k: ImportDscMapItem(**v) for k, v in self.importDscMap.items()} 
+        # self.importDscMap = [for k, v in self.importDscMap.items():
+        #     k: ImportDscMapItem(**v)
+        # } 
+        result = {}
+
+        for k, v in self.importDscMap.items():
+            # v = [ImportDscMapItem]
+            result[k] = [ImportDscMapItem(**i) for i in v]
+
+        self.importDscMap = result
         self.failedItems = [RelatedItemData(**i) for i in self.failedItems]
